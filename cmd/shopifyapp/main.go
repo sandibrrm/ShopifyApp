e40ffbf0be21e994eb8450c3bd2ce27a68fab005
Sandi <43517564+sandibrrm@users.noreply.github.com>
2025-08-07 05:10:09 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/shopifyapp/main.go
@@ -0,0 +1,23 @@
+// cmd/shopifyapp/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "shopifyapp/internal/shopifyapp"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := shopifyapp.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
